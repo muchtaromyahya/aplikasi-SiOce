53ac91e757a0c5a9b3cf8df4abd390f4e73b0cee
andikaarif <andikapradanaarif@gmail.com>
2020-12-13 16:11:51 +0700

    feat: setup environment

---
+++ src/App.js
@@ -0,0 +1,49 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import './App.css';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import routes from './config/routes';
+import { isUserAuthenticated } from './utils/cookie';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (isUserAuthenticated()) {
+          return <Component />;
+        }
+        return <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {routes.map((route) => {
+          if (route.isPublic) {
+            return (
+              <Route
+                path={route.path}
+                component={route.component}
+                key={route.path}
+              />
+            );
+          }
+          return (
+            <PrivateRoute
+              path={route.path}
+              component={route.component}
+              key={route.path}
+            />
+          );
+        })}
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+// import { Home } from '../pages';
+
+const routes = [
+  // {
+  //   path: '/',
+  //   component: Home,
+  //   isPublic: true,
+  // },
+];
+
+export default routes;
