7e274e8251d672b322e09edd2143f5180259074a
Yoga Prianjaya <68526965+yogaprianjaya@users.noreply.github.com>
2020-12-17 21:11:43 +0700

    posting & comment

--- src/pages/comments/index.js
+++ src/pages/comments/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import './style.css';
 
+const list = [
+  {
+      id: '1',
+      name: 'Yoga',
+      time: '12-10-2020 17:20',
+      text: 'Maaf izin bertanya untuk soal no 3 itu seperti apa ya?'
+  },
+  {
+      id: '2',
+      name: 'Prianjaya',
+      time: '21-01-2020 20:17',
+      text: 'Saya juga ingin bertanya mengenai pelaporan apakah pengumpulannya hanya 1 orang dari tiap kelompok?'
+  }
+];
+
 const Comments = () => {
-  return (
-    <div>
-      <h1>Comments</h1>
+  const dataComent = list.map((data) => {
+    return (
+      <div key={data.id} className="comentBox">
+        <div className="boxMain">
+          <div className="boxProfile">
+            <h1>{data.name}</h1>
+            <p>{data.time}</p>
+          </div>
+          <div className="boxText">
+            <p>{data.text}</p>
+          </div>
+        </div>
+      </div>
+    );
+});
+
+return (
+  <div id="comentContainer">
+    { dataComent }
+
+    <div id="formComent">
+      <div className="formInput">
+        <textarea value="Silahkan Masukkan Komentar Anda ..." />
+        <input type="submit" value="Kirim" />
+      </div>
     </div>
-  );
+
+  </div>
+);
 };
 
 export default Comments;
--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -2,11 +2,36 @@ import React from 'react';
 import './style.css';
 
 const Posts = () => {
-  return (
-    <div>
-      <h1>Posts</h1>
-    </div>
-  );
+  const dataPosting = list.map((data) => {
+    return (
+      <div className="boxPosting" key={data.id}>
+        <div className="boxMain">
+          <div className="boxProfile">
+            <h1>{data.name}</h1>
+            <p>{data.time}</p>
+          </div>
+          <div className="action">
+            <div className="btnAction">
+              <img src={edit} alt="edit" />
+            </div>
+            <div className="btnAction">
+              <img src={hapus} alt="delete" />
+            </div>
+          </div>
+        </div>
+        <div className="boxText">
+          <p>{data.text}</p>
+        </div>
+      </div>
+    );
+});
+
+return (
+  <div id="containerPosting">
+    {dataPosting}
+    <Coments />
+  </div>
+);
 };
 
 export default Posts;
