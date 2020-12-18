c844a8de4da1769e5e0b3b34c1d707db58f36169
Yoga Prianjaya <68526965+yogaprianjaya@users.noreply.github.com>
2020-12-19 01:10:24 +0700

    Api

--- src/config/rest.js
+++ src/config/rest.js
@@ -1,3 +1,4 @@
 export default {
   LOGIN: '/login',
+  COMMENT: '/comment',
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from './home';
 export { default as Classes } from './classes';
-export { default as Comments } from './home';
+export { default as Comments } from './comments';
 export { default as Posts } from './posts';
 export { default as Tasks } from './tasks';
